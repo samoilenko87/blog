c1a3b81795672852797dd8a6901640d36314f9da
iVAN <i.samoilenko87@gmail.com>
2018-04-10 13:13:46 +0300

    Create category.index page

--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -19,6 +19,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 </head>
 <body>
 <div id="app">
@@ -38,7 +40,7 @@
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"
                         role="button" aria-expanded="false">БЛОГ</a>
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Категории</a></li>
+                            <li><a href="{{route('admin.category.index')}}">Категории</a></li>
                             <li><a href="#">Материалы</a></li>
                         </ul>
                     </li>
